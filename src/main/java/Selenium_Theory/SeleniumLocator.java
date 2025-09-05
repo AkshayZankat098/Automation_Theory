f7ed700fb634c64885715331b56a1c6fa1448eee
AkshayZankat098 <akshayzankat999@gmail.com>
2025-09-05 14:51:12 +0530

    5th Sep Company

--- src/main/java/Selenium_Theory/SeleniumLocator.java
+++ src/main/java/Selenium_Theory/SeleniumLocator.java
@@ -2,28 +2,30 @@ package Selenium_Theory;
 
 public class SeleniumLocator {
 
-// ID, Name, ClassName, TagName
-// LinkText: Selenium locator strategy used to locate hyperlinks (<a> tags) based on the exact visible text of the link. It only works with elements that use the HTML <a> tag.
-// Partial LinkText: Instead of full text only required the Partial text from the full text.
+/* ID, Name, ClassName, TagName
+ LinkText: Selenium locator strategy used to locate hyperlinks (<a> tags) based on the exact visible text of the link. It only works with elements that use the HTML <a> tag.
+ Partial LinkText: Instead of full text only required the Partial text from the full text.
 
-// Css Selector:
+ Css Selector:
 
-//Xpath : Syntax for finding any element on the we page using XML path (Location of web elements) expression (DOM structure).
+Xpath : Syntax for finding any element on the we page using XML path (Location of web elements) expression (DOM structure).
 
-//CopyFullXpath : /html/body/app-root/app-menu/div/div/div/div/mx-blocked-users/div[3]/div[1]/div/div/form/div[1]/div[2]/div/mx-server-pick-list-button/form/div/div/div[1]/input
-//CopyXpath : //*[@id="AutoBlockUserID"]
+CopyFullXpath : /html/body/app-root/app-menu/div/div/div/div/mx-blocked-users/div[3]/div[1]/div/div/form/div[1]/div[2]/div/mx-server-pick-list-button/form/div/div/div[1]/input
+CopyXpath : //*[@id="AutoBlockUserID"]
 
-//1) Absolute Xpath / Full Xpath :
-//2) Relative Xpath / Partial Xpath :
+1) Absolute Xpath / Full Xpath :
+2) Relative Xpath / Partial Xpath :
 
-//Syntax of relative Xpath parth01 : //tagname[@attribute='Value']
-//Example : //input[@name='txtUsername'] && //*[@name='txtUsername'] (*= Regular expression and represent of all the tagnames or tags)
+Syntax of relative Xpath parth01 : //tagname[@attribute='Value']
+Example : //input[@name='txtUsername'] && //*[@name='txtUsername'] (*= Regular expression and represent of all the tagnames or tags)
 
-//Methods Present in relative Xpath ::
-//or, and, contains(), starts-with(), text(), chained xpath
-// Contains:         //tagname[contains(@attribute, '')]
-// Starts-with:      //tagname[starts-with(@attribute, '')]
-// Text match:       //tagname[text()='']
-// Chained XPath:    //form[@id='searchbox']//input[@id='search_text'] (Here, <form> is the parent and <input> is the child)
+Methods Present in relative Xpath ::
+or, and, contains(), starts-with(), text(), chained xpath
+ Contains:         //tagname[contains(@attribute, '')]
+ Starts-with:      //tagname[starts-with(@attribute, '')]
+ Text match:       //tagname[text()='']
+ Chained XPath:    //form[@id='searchbox']//input[@id='search_text'] (Here, <form> is the parent and <input> is the child)
+
+ */
 
 }
